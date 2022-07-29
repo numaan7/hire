bcd1bd5dc516df903b049360c1d17d243dc57c7b
numaan7 <101054796+numaan7@users.noreply.github.com>
2022-07-30 00:20:03 +0530

    Update settings.py

--- hr/settings.py
+++ hr/settings.py
@@ -79,6 +79,12 @@ WSGI_APPLICATION = 'hr.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
